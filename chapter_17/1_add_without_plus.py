28e43230d05aee33f8ca3c6e2e70d4192e5a2fc1
Renaud Hartert <91352809+rhartert@users.noreply.github.com>
2023-01-13 20:48:07 +0900

    Add recursive answer to 17.1

--- chapter_17/1_add_without_plus.py
+++ chapter_17/1_add_without_plus.py
@@ -14,12 +14,21 @@ def add(a: int, b: int) -> int:
     return sum
 
 
+def add_recursive(a: int, b: int) -> int:
+    if b == 0:
+        return a
+    sum = a ^ b
+    rest = (a & b) << 1
+    return add_recursive(sum, rest)
+
+
 class Test(unittest.TestCase):
     def test(self):
         for a in range(101):
             for b in range(101):
                 want = a + b
                 assert add(a, b) == want
+                assert add_recursive(a, b) == want
 
 
 if __name__ == "__main__":
